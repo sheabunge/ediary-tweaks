2ae159ea86a0c7986843c4218161928f075b3e99
Shea Bunge <info@bungeshea.com>
2014-03-01 13:45:38 +1100

    Use CSS instead of JS for sidebar icons

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,3 @@ var style = document.createElement('link');
 style.rel = 'stylesheet';
 style.href = chrome.extension.getURL('css/app.min.css');
 (document.head || document.documentElement).appendChild(style);
-
-/**
- * Change an element into a FontAwesome icon
- */
-var changeToIcon = function( el, icon, title, classes ) {
-
-	var html = '<i class="fa fa-' + icon;
-
-	if ( classes ) {
-		html += ' ' + classes;
-	}
-
-	if ( title ) {
-		html += '" title="' + title;
-	}
-
-	html += '"></i>';
-	el.outerHTML = html;
-};
-
