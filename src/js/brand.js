2c468de45cb0db7ed353645f4765fed9f6386927
Shea Bunge <info@bungeshea.com>
2014-03-01 13:49:26 +1100

    Use CSS for brand icon instead of JS

--- src/js/brand.js
+++ src/js/brand.js
@@ -4,5 +4,3 @@ var brand = document.querySelector( '.brand' );
 // Append sub-brand to end of brand
 brand.textContent += ' ' + document.querySelector( '.brand-small' ).textContent;
 
-// Prepend cogs icon
-brand.innerHTML = '<i class="fa fa-cogs"></i> ' + brand.innerHTML;
