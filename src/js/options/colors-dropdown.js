5fc873554215cc50c39084bf1314b32358916514
Shea Bunge <info@bungeshea.com>
2014-03-28 14:18:47 +1100

    Fix color names

--- src/js/options/colors-dropdown.js
+++ src/js/options/colors-dropdown.js
@@ -1,13 +1,7 @@
 var colors = [
-
-	// Cool
-	'aqua', 'blue', 'navy', 'teal', 'green', 'olive', 'lime',
-
-	// Warm
-	'yellow', 'orange', 'red', 'fuchsia', 'purple', 'maroon',
-
-	// Gray Scale
-	'white', 'silver', 'gray', 'black'
+	'blue', 'cyan', 'navy', 'teal', 'green', 'olive', 'lime',
+	'yellow', 'orange', 'red', 'magenta', 'purple', 'maroon',
+	'white', 'silver', 'grey', 'black'
 ];
 
 /** Add capitalize method to strings */
@@ -18,7 +12,7 @@ String.prototype.capitalize = function() {
 // Build colors dropdown
 var colorsDropdown = '';
 
-for ( var i = colors.length - 1; i >= 0; i-- ) {
+for ( var i = 0; i < colors.length; i++ ) {
 	colorsDropdown += '<option value="' + colors[i] + '">';
 	colorsDropdown += colors[i].capitalize() + '</option>\n';
 }
